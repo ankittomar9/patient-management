5debff74267cf34bfda25d3959c465b682bd0abf
tomar <tomarankit78@gmail.com>
2025-09-12 14:27:16 +0530

    Patient Service Information

--- patient-service/src/main/java/com/company/patientservice/dto/PatientRequestDTO.java
+++ patient-service/src/main/java/com/company/patientservice/dto/PatientRequestDTO.java
@@ -4,7 +4,11 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 public class PatientRequestDTO {
     @NotBlank
     @Size(max = 100,message="Name should be less than 100 characters")
