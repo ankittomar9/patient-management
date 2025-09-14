d2eb6c61f3e19fd8edf78857946e360cc94189a4
tomar <tomarankit78@gmail.com>
2025-09-14 15:55:55 +0530

    Patient Service Information

--- patient-service/src/main/java/com/company/patientservice/service/PatientService.java
+++ patient-service/src/main/java/com/company/patientservice/service/PatientService.java
@@ -2,6 +2,7 @@ package com.company.patientservice.service;
 
 import com.company.patientservice.dto.PatientRequestDTO;
 import com.company.patientservice.dto.PatientResponseDTO;
+import com.company.patientservice.exception.EmailAlreadyExistsException;
 import com.company.patientservice.mapper.PatientMapper;
 import com.company.patientservice.model.Patient;
 import com.company.patientservice.repository.PatientRepository;
@@ -9,6 +10,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
@@ -25,9 +27,20 @@ public class PatientService {
   }
 
   public PatientResponseDTO createPatient(PatientRequestDTO patientRequestDTO){
+    if(patientRepository.existsByEmail(patientRequestDTO.getEmail())){
+      throw new EmailAlreadyExistsException("Patient already exists" + " with this email " + patientRequestDTO.getEmail());
+    }
+
     Patient newpatient=patientRepository.save(PatientMapper.toModel(patientRequestDTO));
 
     return PatientMapper.toDTO(newpatient);
+  }
+
+  public PatientResponseDTO updatingPatient(UUID id, PatientRequestDTO patientRequestDTO){
+      Patient patient = patientRepository.findById(id)
+              .orElseThrow(() -> new PatientNotFoundException("Patient not found" +
+              " with this id " + id));
+
   }
 
 }
