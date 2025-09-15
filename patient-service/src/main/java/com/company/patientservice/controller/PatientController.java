9465e7b1b7e7b1569ccf5ee12707190f5246ba45
tomar <tomarankit78@gmail.com>
2025-09-15 20:01:39 +0530

    Patient Service Information

--- patient-service/src/main/java/com/company/patientservice/controller/PatientController.java
+++ patient-service/src/main/java/com/company/patientservice/controller/PatientController.java
@@ -29,7 +29,7 @@ public class PatientController {
 
     }
     @PostMapping
-    public ResponseEntity<PatientResponseDTO> createPatient(@Valid @RequestBody PatientRequestDTO patientRequestDTO){
+    public ResponseEntity<PatientResponseDTO> createPatient(@Validated({Default.class,CreatePatientValidationGroup.class}) @RequestBody PatientRequestDTO patientRequestDTO){
       PatientResponseDTO patientResponseDTO = patientService.createPatient(patientRequestDTO);
       return ResponseEntity.ok().body(patientResponseDTO);
     }
