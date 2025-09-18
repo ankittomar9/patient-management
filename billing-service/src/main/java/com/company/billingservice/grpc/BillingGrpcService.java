a7d6b1db190a98beb01bc2f6682ee4b5f47d5638
tomar <tomarankit78@gmail.com>
2025-09-18 22:29:35 +0530

    Billing Service Using PrototBuf GRPC

---
+++ billing-service/src/main/java/com/company/billingservice/grpc/BillingGrpcService.java
@@ -0,0 +1,17 @@
+package com.company.billingservice.grpc;
+
+import billing.BillingServiceGrpc;
+import io.grpc.stub.StreamObserver;
+import net.devh.boot.grpc.server.service.GrpcService;
+
+@GrpcService
+public class BillingGrpcService extends BillingServiceGrpc.BillingServiceImplBase {
+
+@Override
+    public void createBillingAccount(billing.BillingRequest billingRequest,
+                                     StreamObserver<BillingRespone>responseObserver){
+
+
+}
+
+}
