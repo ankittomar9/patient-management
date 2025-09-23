a5132321739af63f0960a99577e87dc15caa3f75
tomar <tomarankit78@gmail.com>
2025-09-23 12:10:35 +0530

    User Service Implementation

---
+++ auth-service/src/main/java/com/company/authservice/controller/AuthController.java
@@ -0,0 +1,44 @@
+package com.company.authservice.controller;
+
+import com.company.authservice.dto.LoginRequestDTO;
+import com.company.authservice.dto.LoginResponseDTO;
+import com.company.authservice.service.AuthService;
+import io.swagger.v3.oas.annotations.Operation;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
+
+@RestController
+//@RequestMapping("/api/auth")
+public class AuthController {
+    private final AuthService authService;
+
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
+
+
+@Operation(summary = "Generate token on user login")
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponseDTO> login
+        (@RequestBody LoginRequestDTO loginRequestDTO){
+
+       Optional<String> tokenOptional=authService.authenticate(loginRequestDTO);
+       if(tokenOptional.isEmpty()){
+           return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+       }
+
+       String token=tokenOptional.get();
+       return ResponseEntity.ok(new LoginResponseDTO(token));
+
+    }
+
+
+
+
+}
