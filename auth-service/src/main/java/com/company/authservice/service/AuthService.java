8a0d6e85e8bb71bdd8ae20f51b86d10b0911852d
tomar <tomarankit78@gmail.com>
2025-09-26 00:04:09 +0530

    Authorization Service Implementation

--- auth-service/src/main/java/com/company/authservice/service/AuthService.java
+++ auth-service/src/main/java/com/company/authservice/service/AuthService.java
@@ -2,6 +2,8 @@ package com.company.authservice.service;
 
 import com.company.authservice.dto.LoginRequestDTO;
 import com.company.authservice.model.User;
+import com.company.authservice.util.JwtUtil;
+import io.jsonwebtoken.JwtException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -11,24 +13,34 @@ import java.util.Optional;
 public class AuthService {
     private final UserService userService;
     private final PasswordEncoder passwordEncoder;
-//    private final JwtUtil jwtUtil;
+   private final JwtUtil jwtUtil;
 
 
-    public AuthService(UserService userService, PasswordEncoder passwordEncoder) {
+    public AuthService(UserService userService, PasswordEncoder passwordEncoder, JwtUtil jwtUtil) {
         this.userService = userService;
         this.passwordEncoder = passwordEncoder;
+        this.jwtUtil = jwtUtil;
     }
 
 
     public Optional<String> authenticate(LoginRequestDTO loginRequestDTO) {
         Optional<String> token =userService
                 .findByEmail(loginRequestDTO.getEmail())
-                .filter(u->passwordEncoder.matches(loginRequestDTO.getPassword(),
-                        u.getPassword())
-                        .map(u- jwtUtil.generateToken(u.getEmail(),u.getRole()));
+                .filter(u-> passwordEncoder.matches(loginRequestDTO.getPassword(),
+                        u.getPassword()))
+                        .map(u-> jwtUtil.generateToken(u.getEmail(),u.getRole()));
 
                 return token;
 
     }
 
+    public boolean validateToken(String token) {
+        try{
+            jwtUtil.validateToken(token);
+            return true;
+        }catch (JwtException e){
+            return false;
+        }
+
+    }
 }
